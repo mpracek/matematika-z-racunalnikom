1f3ab003a033f31bf6631adef01311d63eb746d1
mpracek299@gmail.com <mpracek299@gmail.com>
2020-03-03 16:36:07 +0100

    Postavil server, ui, zbral nekaj knjiznjic Dodal template serverja, ui, za osnovo

---
+++ shiny/ui.R
@@ -0,0 +1,36 @@
+#Datoteka za uporabniski umesnik    115 120
+
+#zazenemo potrebne datoteke
+source("../libraries/lib.r")
+
+
+#samo primer
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
